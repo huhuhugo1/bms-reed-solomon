38a18ad49e0942a08204ad4250d5adf6be039960
Juraj Kubiš <jurkokubis@gmail.com>
2018-12-16 20:33:51 +0100

    Refactoring.

--- decoder.cc
+++ decoder.cc
@@ -3,6 +3,9 @@
 #include <iostream>
 #include <cmath>
 
+// External library from https://github.com/pjkundert/ezpwd-reed-solomon/
+#include "rs"
+
 #include "interleaver.tt"
 #include "outputFileMapper.tt"
 
@@ -16,15 +19,23 @@ constexpr int N = 255;
 constexpr int K = 147;
 
 int main(int argc,char *argv[]) {
+  try {
+    // Check arguments
+    if (argc != 2)
+      throw std::runtime_error("[Decoder] Missing arguments!");
+
+    // Prepare output file name
     std::string output_filename = argv[1];
     output_filename += ".ok";
+
     const size_t wr_size = filesize(argv[1]);
     const size_t B = wr_size - (N - 1) * std::ceil((double)wr_size / N);
     const size_t S = N * std::ceil((double)wr_size / N) - wr_size;
     const size_t orig_size = wr_size - (N - K) * (B+ S);
 
-    std::array<char, N> block;
     Interleaver<N, K> interleaver(argv[1], orig_size);
+    std::array<char, N> block;
+
     OutputFileMapper mapper(output_filename, orig_size);
     ezpwd::RS<N, K> decoder;
     size_t tail = orig_size % interleaver.NUM_OF_BLOCKS;
@@ -35,12 +46,14 @@ int main(int argc,char *argv[]) {
         if (i < tail) idx += interleaver.NUM_OF_BLOCKS;
         block[K+j] = interleaver.FILE.CONTENT[idx];
       }
-      //memcpy(block.data() + K, interleaver.FILE.CONTENT + orig_size + i * (N - K), N - K);
       decoder.decode(block);
-      for (size_t line = 0; line < x; ++line) {
-        mapper.CONTENT[line * (B + S) + i] = block[line];
-      }
+      mapper.write(block, x, 0, i, B + S);
     }
     
     return 0; 
+  
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << std::endl;
+    return 1;
+  }
 }
--- encoder.cc
+++ encoder.cc
@@ -1,41 +1,48 @@
 #include <string>
 
+// External library from https://github.com/pjkundert/ezpwd-reed-solomon/
 #include "rs"
+
 #include "interleaver.tt"
 #include "outputFileMapper.tt"
 
+// Size of ReedSolomon block
 constexpr size_t N = 255;
+// Number of non parity symbols per block
 constexpr size_t K = 147;
 
 int main(int argc, char *argv[]) {
-  if (argc != 2)
-    throw std::runtime_error("Missing arguments");
+  try {  
+    // Check arguments
+    if (argc != 2)
+      throw std::runtime_error("[Encoder] Missing arguments!");
 
-  Interleaver<N, K> interleaver(argv[1]);
-  std::array<char, N> block;
-
-  std::string output_filename = argv[1];
-  output_filename += ".out";
-
-  size_t out_size = interleaver.FILE.SIZE + interleaver.NUM_OF_BLOCKS * (N- K );
-  OutputFileMapper mapper(output_filename, out_size);
-  for (size_t i = 0; i < interleaver.FILE.SIZE; ++i) {
-    mapper.CONTENT[i] = interleaver.FILE.CONTENT[i];
-  }
-
-  ezpwd::RS<N, K> encoder;
-  size_t block_cnt = 0;
-  size_t tail = interleaver.FILE.SIZE % interleaver.NUM_OF_BLOCKS;
-  while (interleaver.getBlock(block)) {
-    encoder.encode(block);
-    for (size_t i = 0; i < N-K; ++i) {
-      size_t idx = interleaver.FILE.SIZE + i * interleaver.NUM_OF_BLOCKS + block_cnt - tail;
-      if (block_cnt < tail) idx += interleaver.NUM_OF_BLOCKS;
-      mapper.CONTENT[idx] = block[K + i];
-    }
-    //out.write(block.data() + K, N-K);
-    ++block_cnt;
-  }
+    // Prepare output file name
+    std::string output_filename = argv[1];
+    output_filename += ".out";
     
-  return 0;
+    Interleaver<N, K> interleaver(argv[1]);
+    std::array<char, N> block;
+
+    size_t out_size = interleaver.FILE.SIZE + interleaver.NUM_OF_BLOCKS * (N - K);
+    OutputFileMapper mapper(output_filename, out_size);
+    mapper.write(interleaver.FILE.CONTENT, interleaver.FILE.SIZE);
+
+    ezpwd::RS<N, K> encoder;
+    size_t tail = interleaver.FILE.SIZE % interleaver.NUM_OF_BLOCKS;
+    for (size_t block_cnt = 0; interleaver.getBlock(block); ++block_cnt) {
+      encoder.encode(block);
+      
+      size_t offset = interleaver.FILE.SIZE - tail + block_cnt;
+      if (block_cnt < tail) 
+        offset += interleaver.NUM_OF_BLOCKS;
+      mapper.write(block, N-K, K, offset, interleaver.NUM_OF_BLOCKS); 
+    }
+      
+    return 0;
+
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << std::endl;
+    return 1;
+  }
 }
